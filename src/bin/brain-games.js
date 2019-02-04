2b93beca9863352159ba0d2ad891d42298a0e3d5
Petr Zyuzev <petrzyzevja@yandex.ru>
2019-02-05 00:32:45 +0300

    project update to step 2

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
+import index from '../index';
+
 console.log('Welcome to the Brain Games!');
+index();
---
+++ src/index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+export default () => {
+  const actual = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${actual}`);
+};
